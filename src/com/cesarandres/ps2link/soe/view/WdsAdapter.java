0da40168e954cec6864efc318218248f7c6f800d
Cesar Ramirez <cesar.ramsan@gmail.com>
2013-09-17 03:22:44 -0500

    Release 2.5.0 with WDS stats viewer

--- src/com/cesarandres/ps2link/soe/view/WdsAdapter.java
+++ src/com/cesarandres/ps2link/soe/view/WdsAdapter.java
@@ -11,6 +11,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,9 +33,11 @@ import com.cesarandres.ps2link.soe.SOECensus.Verb;
 import com.cesarandres.ps2link.soe.content.CharacterEvent;
 import com.cesarandres.ps2link.soe.content.Faction;
 import com.cesarandres.ps2link.soe.content.WDS_Stat;
+import com.cesarandres.ps2link.soe.content.WDS_Stat.Day;
 import com.cesarandres.ps2link.soe.content.item.IContainDrawable;
 import com.cesarandres.ps2link.soe.content.response.Item_list_response;
 import com.cesarandres.ps2link.soe.content.response.Server_response;
+import com.cesarandres.ps2link.soe.util.Collections;
 import com.cesarandres.ps2link.soe.util.QueryString;
 import com.cesarandres.ps2link.soe.util.Collections.PS2Collection;
 import com.cesarandres.ps2link.soe.util.QueryString.SearchModifier;
@@ -55,6 +58,21 @@ public class WdsAdapter extends BaseAdapter {
 
 	public WdsAdapter(Context context, ArrayList<WDS_Stat> wdsStats) {
 		this.mInflater = LayoutInflater.from(context);
+		for (int i = 0; i < wdsStats.size(); i++) {
+			if (!wdsStats.get(i).getStat_name().startsWith("EmpireScore.WDS01")) {
+				wdsStats.remove(i);
+				i--;
+			} else if (wdsStats.get(i).getWorld_id().equals("19") || wdsStats.get(i).getWorld_id().equals("3")) {
+				wdsStats.remove(i);
+				i--;
+			}
+		}
+		java.util.Collections.sort(wdsStats);
+		int iterations = wdsStats.size() / 3;
+		for (int i = 0; i < (iterations); i++) {
+			wdsStats.add(i * 4, new WDS_Stat());
+		}
+
 		this.wdsStats = wdsStats;
 		requestTable = new Hashtable<View, GsonRequest>(20);
 		icon_vs = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_faction_vs);
@@ -86,9 +104,7 @@ public class WdsAdapter extends BaseAdapter {
 
 			holder = new ViewHolder();
 			holder.faction = (ImageView) convertView.findViewById(R.id.imageViewWdsStatItemFaction);
-			holder.server = (TextView) convertView.findViewById(R.id.TextViewWdsStatItemServer);	
-			holder.name = (TextView) convertView.findViewById(R.id.TextViewWdsStatItemName);
-			holder.total = (TextView) convertView.findViewById(R.id.TextViewWdsStatItemTotal);
+			holder.server = (TextView) convertView.findViewById(R.id.TextViewWdsStatItemServer);
 			holder.today = (TextView) convertView.findViewById(R.id.TextViewWdsStatItemToday);
 			holder.week = (TextView) convertView.findViewById(R.id.TextViewWdsStatItemWeek);
 			holder.month = (TextView) convertView.findViewById(R.id.TextViewWdsStatItemMonth);
@@ -101,8 +117,24 @@ public class WdsAdapter extends BaseAdapter {
 			}
 		}
 
-		updateServer(getItem(position).getWorld_id(), PS2Collection.WORLD, holder.server, convertView);
-		
+		if (position % 4 == 0) {
+			if (getItem(position).getWorld_name() == null) {
+				updateServer(getItem(position + 1).getWorld_id(), PS2Collection.WORLD, holder.server, position, convertView);
+			} else {
+				holder.server.setText(getItem(position).getWorld_name());
+			}
+			holder.faction.setImageBitmap(null);
+			holder.server.setVisibility(View.VISIBLE);
+			holder.today.setText("Today:");
+			holder.week.setText("Week:");
+			holder.month.setText("Season:");
+			holder.today.setGravity(Gravity.NO_GRAVITY);
+			holder.week.setGravity(Gravity.NO_GRAVITY);
+			holder.month.setGravity(Gravity.NO_GRAVITY);
+
+			return convertView;
+		}
+		holder.server.setVisibility(View.INVISIBLE);
 		if (getItem(position).getFaction().equals(VS)) {
 			holder.faction.setImageBitmap(icon_vs);
 		} else if (getItem(position).getFaction().equals(NC)) {
@@ -111,37 +143,38 @@ public class WdsAdapter extends BaseAdapter {
 			holder.faction.setImageBitmap(icon_tr);
 		}
 
-		holder.name.setText(getItem(position).getStat_name());
-		holder.total.setText(getItem(position).getAll_time());
 		holder.today.setText(Integer.toString(getItem(position).getToday()));
 		holder.week.setText(Integer.toString(getItem(position).getThisWeek()));
-		holder.month.setText(Integer.toString(getItem(position).getThisMonth()));		
-		
+		holder.month.setText(Integer.toString(getItem(position).getThisMonth()));
+		holder.today.setGravity(Gravity.CENTER_HORIZONTAL);
+		holder.week.setGravity(Gravity.CENTER_HORIZONTAL);
+		holder.month.setGravity(Gravity.CENTER_HORIZONTAL);
+
 		return convertView;
 	}
 
 	static class ViewHolder {
 		ImageView faction;
-		TextView name;
 		TextView server;
-		TextView total;
 		TextView today;
 		TextView week;
 		TextView month;
 	}
 
-	private void updateServer(String world_id, PS2Collection collection, final TextView name, View view) {
+	private void updateServer(String world_id, PS2Collection collection, final TextView name, final int position, View view) {
 
 		URL url;
 		try {
 			url = SOECensus.generateGameDataRequest(Verb.GET, Game.PS2V1, PS2Collection.WORLD, "",
 					QueryString.generateQeuryString().AddComparison("world_id", SearchModifier.EQUALS, world_id));
-
+			name.setText("Loading...");
 			Listener<Server_response> success = new Response.Listener<Server_response>() {
 				@Override
 				public void onResponse(Server_response response) {
 					if (response.getWorld_list().size() > 0) {
-						name.setText(response.getWorld_list().get(0).getName().getEn());
+						String worldName = response.getWorld_list().get(0).getName().getEn();
+						getItem(position).setWorld_name(worldName);
+						name.setText(worldName);
 					} else {
 						name.setText("UNKNOWN");
 					}
